1689d36e888880bb138a61c5a944eae7544cfa78
Duck <roanlake@gmail.com>
2019-06-11 11:25:59 +0800

    add information of different pages

--- cnt_page.py
+++ cnt_page.py
@@ -6,16 +6,19 @@ def merge(cfile,pfile):
     plines=page.readlines()
     cnt.close()
     page.close()
-    for i in range(len(clines)):
-        clines[i]=clines[i].rstrip()+'\t'+plines[i]
-    newc=open(cfile,'w')
-    newc.writelines(clines)
-    newc.close()
+    c=len(clines)
+    p=len(plines)
+    if c!=p:
+        print (f"content lines is {c},page lines is {p}")
+    else:
+        for i in range(len(clines)):
+            clines[i]=clines[i].rstrip()+'\t'+plines[i]
+            newc=open(cfile,'w')
+            newc.writelines(clines)
+            newc.close()
     return
 import sys
 try:
     merge(sys.argv[1],sys.argv[2])
 except:
     print (sys.exc_info()[1])
-else:
-    print ("Succeed!")
