6e7e5a82ab40f062d65006fc5e78e25c85dd9698
atcg16569 <atcg16569@users.noreply.github.com>
2024-10-13 08:17:55 +0800

    Update ankiCompare.py

--- ankiCompare.py
+++ ankiCompare.py
@@ -1,31 +1,36 @@
 import re
 import sys
+import os
 
 
 def anki_compare(file):
     f = open(file, encoding='utf-8')
     p = re.compile(r"\d{4}")
+    count = 0
     for line in f:
         data = line.split(';')
         year = p.match(data[1]).group()
         mid = data[0]
         checkFile = year + ".txt"
-        try:
+        checkPath = os.getcwd() + "\\" + checkFile
+        if os.path.isfile(checkPath):
             with open(checkFile) as op:
                 checkContent = op.read()
             if mid in checkContent:
                 print(f"{mid} in {checkFile}")
             else:
-                print(f"{mid} not in {checkFile}")
-                wf = open(checkFile, "a")
-                if not checkContent.endswith("\n"):
-                    wf.write("\n")
-                    print("add \\n")
-                wf.write(line)
-                wf.close()
+                # print(f"{mid} not in {checkFile}")
+                with open(checkFile, "a") as wf:
+                    if not checkContent.endswith("\n"):
+                        wf.write("\n")
+                        # print("add \\n")
+                    wf.write(line)
+                count += 1
                 print(f"write {mid} to {checkFile}")
-        except FileNotFoundError:
+        else:
             print(f"{checkFile} not existed")
+    print(f"total wrote {count}")
+    f.close()
 
 
 if __name__ == '__main__':
