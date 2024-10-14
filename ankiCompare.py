ca106d636ab351f86905647b64c6f7750151a32b
atcg16569 <atcg16569@users.noreply.github.com>
2024-10-14 20:14:38 +0800

    Update ankiCompare.py

--- ankiCompare.py
+++ ankiCompare.py
@@ -2,42 +2,58 @@ import re
 import sys
 import os
 
+cwd = os.getcwd()
+seriesD = cwd+"/series/"
+yearsD = cwd+"/years/"
+
 
 def anki_compare(file):
-    addF = open(file, encoding='utf-8')
+    addF = open(seriesD+file, encoding='utf-8')
     p = re.compile(r"\d{4}")
     count = 0  # 写入
     t = 0  # 总数
     et = 0  # 已有
     noFileT = 0  # 无文件
-    cwd = os.getcwd()
-    cwdFs = os.listdir(cwd)
     for line in addF:
         t += 1
         data = line.split(';')
         year = p.match(data[1]).group()
         mid = data[0]
-        checkFp = re.compile(year+r"(\.\d+)*\.txt")
-        exList = []  # 匹配存在目录 
+        checkFp = re.compile(year + r"(\.\d+)*\.txt")
+        exList = []  # 匹配存在目录
         exContent = ""  # 合并检查内容
         writePath = ""  # 写入目标
+        cwdFs = os.listdir(yearsD)
         for cwdF in cwdFs:
             if checkFp.match(cwdF):
-                checkPath = cwd + "/" + cwdF
+                checkPath = yearsD + cwdF
                 if checkPath not in exList:
                     exList.append(checkPath)
-        if len(exList) != 0:
+        exFileN = len(exList)
+        if exFileN != 0:
+            up500 = 0
             for exF in exList:
                 if os.path.isfile(exF):
-                    exOp = open(exF)
-                    exContent += exOp.read()
-                    if len(writePath) == 0 and len(exOp.readlines()) < 500:
+                    with open(exF) as exOp:
+                        lin = len(exOp.readlines())
+                    with open(exF) as exRp:
+                        exContent += exRp.read()
+                    if len(writePath) == 0 and lin < 500:
                         writePath += exF
-                    exOp.close()
-            if mid not in exContent and writePath != "":
+                    if lin >= 500:
+                        up500 += 1
+            if up500 == exFileN and mid not in exContent:
+                writePath += f"{yearsD}{year}.{exFileN}.txt"
+                newT = open(writePath, "x")
+                newT.write(line)
+                newT.close()
+                count += 1
+                print(f"write {mid} to {writePath}")
+            elif mid not in exContent and writePath != "":
                 read = open(writePath)
                 newL = open(writePath, "a")
-                if not read.read().endswith("\n"):
+                rCon = read.read()
+                if not rCon.endswith("\n") and len(rCon) > 0:
                     newL.write("\n")
                 else:
                     newL.write(line)
@@ -47,10 +63,10 @@ def anki_compare(file):
                 print(f"write {mid} to {writePath}")
             else:
                 et += 1
-                print(f"{mid} in {exList}")
+                print(f"{mid} In file {year}")
         else:
             noFileT += 1
-            print(f"{mid} no file")
+            print(f"{mid} no file {year}")
     print(f"total {t},wrote {count},exist {et},no file {noFileT}")
     addF.close()
 
